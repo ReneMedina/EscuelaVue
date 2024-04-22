0a1bcbb77fca714276d8a9f20b313ffb34e543b3
ReneMedina <138516015+ReneMedina@users.noreply.github.com>
2024-04-22 09:44:35 -0700

    primer commit
    primer commit

---
+++ Escuela-vue.Web/Controllers/AlumnosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Escuela_vue.Web.Controllers
+{
+	public class AlumnosController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Escuela-vue.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Escuela_vue.Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
